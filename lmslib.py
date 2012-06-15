58632ba3852e6ad4c7ac397840ea7c360f979e6d
Wesley Hansen <wes@ridersdiscount.com>
2012-06-15 11:32:39 -0400

    Added GPLv3 License

--- lmslib.py
+++ lmslib.py
@@ -1,4 +1,17 @@
 #This Python file uses the following encoding: utf-8
+#    This module is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.#
+#
+#    This module is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 the lmslib module contains classes that build the eBay LMS API call requests
 and has the functionality to send them to the eBay server( sandbox and production ).
